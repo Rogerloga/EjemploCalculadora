0c024d6040403656fb03faa14349fd51e01b0f76
Rogerloga <rogercr814@gmail.com>
2021-05-10 17:05:01 +0200

    añadimos la multiplicacion

--- src/EjemploCalculadora/Calculadora.java
+++ src/EjemploCalculadora/Calculadora.java
@@ -49,9 +49,6 @@ public class Calculadora {
     public double multiplicacion () {
         return (this.num1 * this.num2);
     }
-    public double division () {
-       return (this.num1 / this.num2);
-    }
 
 
 // GETTERS & SETTERS
